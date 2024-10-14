b1751a43b9a45bbf511884e3cd8559e3e44f6250
ShigeruOda <seq.drug@gmail.com>
2024-10-14 10:43:59 +0900

    v4

---
+++ sql/v4/5.crud.sql
@@ -0,0 +1,15 @@
+USE db1;
+-- 1. 最初の10件のデータをSELECT
+SELECT * FROM table1 WHERE id BETWEEN 1 AND 10;
+
+-- 2. 最初の10件の年齢を+1するUPDATE
+UPDATE table1 SET age = age + 1 WHERE id BETWEEN 1 AND 10;
+
+-- 3. UPDATE後のデータをSELECT
+SELECT * FROM table1 WHERE id BETWEEN 1 AND 10;
+
+-- 4. 最初の10件をDELETE
+DELETE FROM table1 WHERE id BETWEEN 1 AND 10;
+
+-- 5. DELETE後のデータをSELECT（データが削除されているか確認）
+SELECT * FROM table1 WHERE id BETWEEN 1 AND 10;
